7b1ff04126e839a4f9dddacd113da3eb66b3e8a2
Abcia2 <gianluca.arobaleno@gmail.com>
2024-12-17 12:24:23 +0100

    Aggiungere i file di progetto.

---
+++ Sensore Temperatura/SensoreTemperatura.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensore_Temperatura
+{
+    public delegate void RaffreddamentoHandler(double Temperatura);
+    internal class SensoreTemperatura
+    {
+
+        // Proprietà
+        private double Temperatura;
+        private const double Soglia = 30.0;
+
+        public event RaffreddamentoHandler TemperaturaAlta;
+
+
+        // Costruttori
+        public SensoreTemperatura()
+        {
+            ImpostaTemperature(0);
+        }
+
+        public SensoreTemperatura(double val)
+        {
+            ImpostaTemperature(val);
+        }
+
+
+        // Metodi
+        public string ImpostaTemperature(double val)
+        {
+            string Messaggio = "";
+
+            if(!(val < 55.0 && val > -80.0))
+            {
+                Temperatura = val;
+                Messaggio = $"Nuova temperature; {Temperatura}° C";
+
+                if(Temperatura > Soglia)
+                {
+                    TemperaturaAlta?.Invoke(Temperatura);
+                }
+            }
+            else
+            {
+                Messaggio = "Temperatura inserita non valida";
+            }
+
+            return Messaggio;
+        }
+    }
+}
